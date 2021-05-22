057f171e9b27d338d3af69e8ffb164f1666ff0d3
AshakaE <ashakaegerega@gmail.com>
2021-05-22 10:12:10 +0100

    Add TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import './App.css';
+// import { Provider } from 'react-redux';
+// import { createStore, combineReducers } from 'redux';
+import BooksList from './components/BooksList';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
-}
+// const store = createStore(
+//   combineReducers({ bookState: bookReducer }),
+// );
+
+const App = () => (
+  // <Provider store={store}>
+  <div className="App">
+    <BooksList />
+  </div>
+  // </Provider>
+);
 
 export default App;
